bc87b4c914bc53b70c61db38398874a3b06a2444
sebitse <137815242+sebitse@users.noreply.github.com>
2024-05-21 20:50:15 +0300

    create AppUserTest

---
+++ back/src/test/java/com/example/back/appuser/AppUserTest.java
@@ -0,0 +1,108 @@
+package com.example.back.appuser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppUserTest {
+
+    @Test
+    void getAuthorities() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void isAccountNonExpired() {
+    }
+
+    @Test
+    void isAccountNonLocked() {
+    }
+
+    @Test
+    void isCredentialsNonExpired() {
+    }
+
+    @Test
+    void isEnabled() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void canEqual() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getAppUserRole() {
+    }
+
+    @Test
+    void getLocked() {
+    }
+
+    @Test
+    void getEnabled() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setFirstName() {
+    }
+
+    @Test
+    void setLastName() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void setAppUserRole() {
+    }
+
+    @Test
+    void setLocked() {
+    }
+
+    @Test
+    void setEnabled() {
+    }
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void getLastName() {
+    }
+}
