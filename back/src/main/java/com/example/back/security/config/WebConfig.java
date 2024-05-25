8b73452f98fae0410d65dc58f6cfaa5f7b990f07
sebitse <137815242+sebitse@users.noreply.github.com>
2024-05-25 15:03:48 +0300

    frontend services created

--- back/src/main/java/com/example/back/security/config/WebConfig.java
+++ back/src/main/java/com/example/back/security/config/WebConfig.java
@@ -1,4 +1,22 @@
 package com.example.back.security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200") // URL of your Angular app
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
 }
