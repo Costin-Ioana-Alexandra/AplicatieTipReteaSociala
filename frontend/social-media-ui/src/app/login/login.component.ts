87d34566a48d95b5276caabb1e2a133067c87351
sebitse <137815242+sebitse@users.noreply.github.com>
2024-05-26 00:30:21 +0300

    login module done

--- frontend/social-media-ui/src/app/login/login.component.ts
+++ frontend/social-media-ui/src/app/login/login.component.ts
@@ -1,13 +1,28 @@
+// login.component.ts
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { LoginRequest } from '../models/login-request.model';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [RouterModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginRequest: LoginRequest = { username: '', password: '' };
 
+  constructor(private authService: AuthService) { }
+
+  login() {
+    this.authService.login(this.loginRequest).subscribe(
+      response => {
+        console.log('Login successful:', response);
+        // Handle success, e.g., navigate to another page or store the JWT token
+      },
+      error => {
+        console.error('Login failed:', error);
+        // Handle error, e.g., show an error message
+      }
+    );
+  }
 }
