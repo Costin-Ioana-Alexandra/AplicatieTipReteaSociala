370ff5aca4a19337f8fcf86eb7afdafb65c1a09c
sebitse <137815242+sebitse@users.noreply.github.com>
2024-05-26 00:32:19 +0300

    update in AuthService

--- frontend/social-media-ui/src/app/services/auth.service.ts
+++ frontend/social-media-ui/src/app/services/auth.service.ts
@@ -1,9 +1,23 @@
+// auth.service.ts
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LoginRequest } from '../models/login-request.model';
+import { SignUpRequest } from '../models/signup-request.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private baseUrl = 'http://localhost:8080/api/v1';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(loginRequest: LoginRequest): Observable<any> {
+    return this.http.post(`${this.baseUrl}/login`, loginRequest);
+  }
+
+  signUp(signUpRequest: SignUpRequest): Observable<any> {
+    return this.http.post(`${this.baseUrl}/registration`, signUpRequest);
+  }
 }
