9a3e170b6cd9b9609db8fcffea37cb7de069672e
sebitse <137815242+sebitse@users.noreply.github.com>
2024-05-26 00:32:54 +0300

    signup module done

--- frontend/social-media-ui/src/app/signup/signup.component.ts
+++ frontend/social-media-ui/src/app/signup/signup.component.ts
@@ -1,13 +1,34 @@
+// src/app/components/sign-up/sign-up.component.ts
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { SignUpRequest } from '../models/signup-request.model';
 
 @Component({
-  selector: 'app-signup',
-  standalone: true,
-  imports: [RouterModule],
-  templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  selector: 'app-sign-up',
+  templateUrl: 'signup.component.html',
+  styleUrls: ['signup.component.css']
 })
-export class SignupComponent {
+export class SignUpComponent {
+  signUpRequest: SignUpRequest = { lastname: '', firstname: '', email: '', password: '' };
+  error: string | null = null; // Variable to store error messages, initially set to null
 
+  constructor(private authService: AuthService) { }
+
+  signUp() {
+    // Reset error message
+    this.error = null;
+
+    // Call AuthService to sign up user
+    this.authService.signUp(this.signUpRequest).subscribe(
+      response => {
+        console.log('Sign up successful:', response);
+        // Handle success, e.g., navigate to the login page
+      },
+      error => {
+        console.error('Sign up failed:', error);
+        // Set error message
+        this.error = error.error.message || 'An error occurred during sign up.';
+      }
+    );
+  }
 }
