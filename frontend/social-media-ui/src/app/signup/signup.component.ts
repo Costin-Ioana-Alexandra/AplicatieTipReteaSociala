c1736bdf0684803d19368f5c430d93eab313d755
sebitse <137815242+sebitse@users.noreply.github.com>
2024-05-28 21:51:34 +0300

    signup update in order to perform less bugs

--- frontend/social-media-ui/src/app/signup/signup.component.ts
+++ frontend/social-media-ui/src/app/signup/signup.component.ts
@@ -1,34 +1,54 @@
 // src/app/components/sign-up/sign-up.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
-import { SignUpRequest } from '../models/signup-request.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
-  templateUrl: 'signup.component.html',
-  styleUrls: ['signup.component.css']
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
 })
-export class SignUpComponent {
-  signUpRequest: SignUpRequest = { lastname: '', firstname: '', email: '', password: '' };
-  error: string | null = null; // Variable to store error messages, initially set to null
+export class SignUpComponent implements OnInit {
+  signUpForm: FormGroup;
+  error: string | null = null;
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.signUpForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    });
+  }
 
-  signUp() {
-    // Reset error message
-    this.error = null;
+  ngOnInit(): void {}
 
-    // Call AuthService to sign up user
-    this.authService.signUp(this.signUpRequest).subscribe(
-      response => {
-        console.log('Sign up successful:', response);
-        // Handle success, e.g., navigate to the login page
-      },
-      error => {
-        console.error('Sign up failed:', error);
-        // Set error message
-        this.error = error.error.message || 'An error occurred during sign up.';
-      }
-    );
+  signUp(): void {
+    console.log("Merge?");
+
+    if (this.signUpForm.valid) {
+      const signUpData = this.signUpForm.value;
+      this.error = null;
+      this.authService.signUp(signUpData).subscribe(
+        response => {
+          console.log('Sign up successful:', response);
+          this.router.navigate(['/login']);
+        },
+        error => {
+          console.error('Sign up failed:', error);
+          this.error = error.error.message || 'An error occurred during sign up.';
+        }
+      );
+    }
+  }
+
+  goToLogin(): void {
+    this.router.navigate(['/']);
   }
 }
